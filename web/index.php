8287c7c319d4013d687b5f9dbf52bd50cb549cd4
ChristopheMalo <christof.malo@gmail.com>
2016-02-29 15:14:05 +0100

    feat: coder une nouvelle route dans index

--- web/index.php
+++ web/index.php
@@ -4,8 +4,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function() {
+$app->get('/', function()
+{
     return 'Hello world';
+});
+
+$app->get('/hello/{name}', function($name) use ($app)
+{
+   return 'Hello ' . $app->escape($name); 
 });
 
 $app->run();
